54a50fd4069047b353d91f1fb4b0fc23c0a3ad6d
matixmax <matirton@gmail.com>
2018-08-23 18:31:48 +0200

    Add test preparator to repository

---
+++ statisticAnalys/testPlanRandomCreator.py
@@ -0,0 +1,48 @@
+import os
+import random
+import sys
+
+class TestPlanRandomCreator(object):
+    def __init__(self, blackMinRanges, blackMaxRanges, whiteMinRanges, whiteMaxRanges):
+        self.blackMinRanges = blackMinRanges
+        self.blackMaxRanges = blackMaxRanges
+        self.whiteMinRanges = whiteMinRanges
+        self.whiteMaxRanges = whiteMaxRanges
+        self.remember = []
+
+    def getBlackAndWhite(self):
+        black = []
+        white = []
+        for propIdx in range(0,6):
+            black.append(random.randint(self.blackMinRanges[propIdx], self.blackMaxRanges[propIdx]))
+            white.append(random.randint(self.whiteMinRanges[propIdx], self.whiteMaxRanges[propIdx]))
+        if not (black, white) in self.remember:
+            self.remember.append((black, white))
+            return black, white
+        else:
+            return self.getBlackAndWhite()
+
+    def createRandomTests(self, numberOfTests):
+        random.seed()
+        with open("testPropertiesChangePlan_out.txt", 'w') as file:
+            file.write("pawnsPosFact;knightsPosFact;bishopsPosFact;rocksPosFact;queenPosFact;kingsPosFact\n")
+            for i in range(0, numberOfTests):
+                black, white = self.getBlackAndWhite()
+                file.write("\t;".join(str(el) for el in black) + "\n")
+                file.write("\t;".join(str(el) for el in white) + "\n")
+                file.write("\n")
+     
+blackMinRanges = [1,1,1,1,1,1]
+whiteMinRanges = [1,1,1,1,1,1]
+blackMaxRanges = [10,10,10,10,10,10]
+whiteMaxRanges = [10,10,10,10,10,10]              
+
+if len(sys.argv) > 1:
+    numberOfTests = int(sys.argv[1])
+else:
+    numberOfTests = 100
+
+testCreator = TestPlanRandomCreator(blackMinRanges, blackMaxRanges, whiteMinRanges, whiteMaxRanges)
+testCreator.createRandomTests(numberOfTests)
+
+
---
+++ statisticAnalys/testPreparator.py
@@ -0,0 +1,60 @@
+import os
+import shutil
+import re
+
+class testElement:
+    black = []
+    white = []
+
+class TestPreparator:
+    def __init__(self):
+        os.makedirs("Tests")
+        self.data = self.getLines("testPropertiesChangePlan.txt")
+        self.paramNames = self.getLineWithoutWitespaces(self.data[0]).split(';')
+
+    def getLineWithoutWitespaces(self, line):
+        return re.sub(r'\s+', '', line)
+
+    def makeTests(self):
+        tests = []
+        index = 1; 
+        while index < len(self.data):
+            elem = testElement()
+            elem.black = self.getLineWithoutWitespaces(self.data[index]).split(';')
+            elem.white = self.getLineWithoutWitespaces(self.data[index+1]).split(';')
+            if len(elem.black) != len(self.paramNames) or len(elem.white) != len(self.paramNames):
+                print("wrong param number for line " + str(index))
+            else:
+                tests.append(elem)
+            index = index + 3
+
+        testId = 0;
+        for test in tests:
+            self.makeTest(test, testId)
+            testId+=1
+
+    def getLines(self, path):
+        with open(path, 'r') as plan_file:
+            return plan_file.readlines()
+
+    def setPropertiesForTest(self, propertiesPath, test):
+        with open(propertiesPath, 'w') as file:
+            for idx in range(0, len(self.paramNames)):
+                line = self.paramNames[idx] + ';'
+                line += test.black[idx] + ';'
+                line += test.white[idx] + '\n'
+                file.write(line)
+        
+    def makeTest(self, test, testId):
+        testDirShortPath = os.path.join("Tests", "Test_"+str(testId))
+        os.makedirs(testDirShortPath)
+        shutil.copy2("GraAI.exe", testDirShortPath)
+        self.setPropertiesForTest(os.path.join(testDirShortPath, "properties.txt"), test)
+        currentDir = os.path.realpath('.')
+        os.chdir(testDirShortPath)
+        os.system("GraAI.exe")
+        os.chdir(currentDir)
+
+TestPreparator().makeTests()
+
+
